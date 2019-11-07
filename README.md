5e084949ad03c14d06d58fd995d080354cc24acc
Chirag <patelchirag023@gmail.com>
2019-11-07 15:56:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Test for Drupal 8
+
+## Task 1 - Create a Content Type for Hospitals with following detail:
+
+1. Name
+2. Address
+3. Phone number
+4. Speciality (taxonomy) -> One hospital can have multiple speciality like "Cariology", "Neurology", "Opthamology" etc.
+
+## Task 2 - Create a customer role which will be used by Mobile app users to simply register with their name and email.
+
+## Task 3 - Create a API using Rest API or JSON API module and allow following with this:
+
+1. User registration (API should register user as a customer and role should be assigned to him)
+2. User login
+3. Get Content of Hospital (retrive all content of hospital)
+
+
+
+
---
+++ modules/hospitals/src/Controller/HospitalsController.php
@@ -0,0 +1,32 @@
+<?php
+namespace Drupal\hospitals\Controller;
+
+use Drupal\node\Entity\Node;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\taxonomy\Entity\Term;
+
+class HospitalsController {
+  public function content() {
+    $json_array = array(
+      'data' => array()
+    );
+    $nids = \Drupal::entityQuery('node')->condition('type','hospitals')->execute();
+    $nodes =  Node::loadMultiple($nids);
+    foreach ($nodes as $node) {
+$term = Term::load($node->get('field_speciality')->target_id);
+$tname = $term->getName();
+      $json_array['data'][] = array(
+        'type' => $node->get('type')->target_id,
+        'id' => $node->get('nid')->value,
+        'attributes' => array(
+          'title' =>  $node->get('title')->value,
+          'field_address' =>  $node->get('field_address')->value,
+          'field_phone_number' =>  $node->get('field_phone_number')->value,
+          'field_speciality' =>  $tname,
+         
+        ),
+      );
+    }
+    return new JsonResponse($json_array);
+  }
+}
